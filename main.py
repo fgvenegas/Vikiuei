c39c27b1b3d91f08c6f54e5316f3999ce818b9a4
Christian Meléndez Salinas <camelendez@uc.cl>
2019-06-26 11:39:59 -0400

    Finished ramen model

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import torch.nn as nn
 
 
 class Ramen(nn.Module):
-    def __init__(self, k, batch_size):
+    def __init__(self, k, batch_size, n_classes):
         super().__init__()
         self.k = k
         self.batch_size = batch_size
         self.q_emb = nn.GRU(input_size=300, hidden_size=512, bidirectional=True)
         self.projection = Projector(3584, 1024)
+        self.agg = nn.GRU(input_size=2048, hidden_size=1024, bidirectional=True)
+        self.fc_swish = nn.Linear(2048, 2048)
+        self.fc_output = nn.Linear(2048, n_classes)
 
     def forward(self, imgs, q):
         # Take the concatenated features.
@@ -23,14 +26,26 @@ class Ramen(nn.Module):
         print(f'After projection: {b_reshaped.shape}')
         b = b_reshaped.view(self.batch_size, self.k, -1)
         print(f'Reshape of projection: {b.shape}')
-
+        qb_emb = self.late_fusion(q_emb, b)
+        print(f'qb emb {qb_emb.shape}')
+        _, last_hidden = self.agg(qb_emb.permute(1, 0, 2))
+        agg_emb = torch.cat((last_hidden[0], last_hidden[1]), dim=1)
+        print(f'Agg bigru: {agg_emb.shape}')
+        fc_swish = swish(self.fc_swish(agg_emb))
+        print(f'fc_swish shape: {fc_swish.shape}')
+        fc_output = self.fc_output(fc_swish)
+        print(f'fc_output shape: {fc_output.shape}')
+        output = torch.softmax(fc_output, 1)
+        print(f'Output: {output.shape}')
 
     def early_fusion(self, imgs, q_emb):
         q_emb = q_emb.view(q_emb.shape[0], 1, q_emb.shape[1]).repeat(1, imgs.shape[1], 1)
         print(f'question expanding embedding: {q_emb.shape}')
         return torch.cat((imgs, q_emb), dim=2)
-
-
+    
+    def late_fusion(self, q_emb, b):
+        q_emb = q_emb.view(q_emb.shape[0], 1, q_emb.shape[1]).repeat(1, b.shape[1], 1)
+        return torch.cat((b, q_emb), dim=2)
         
 
 class Projector(nn.Module):
@@ -42,18 +57,18 @@ class Projector(nn.Module):
         self.fc4 = nn.Linear(out_features, out_features)
     
     def forward(self, x):
-        x = self.swish(self.fc1(x))
+        x = swish(self.fc1(x))
         residual = x
-        x = self.swish(self.fc2(x)) + residual
+        x = swish(self.fc2(x)) + residual
         residual = x
-        x = self.swish(self.fc3(x)) + residual
+        x = swish(self.fc3(x)) + residual
         residual = x
-        output = self.swish(self.fc4(x)) + residual
+        output = swish(self.fc4(x)) + residual
         return output
 
 
-    def swish(self, x):
-        return x * torch.sigmoid(x)
+def swish(x):
+    return x * torch.sigmoid(x)
 
 
 
@@ -61,6 +76,7 @@ if __name__ == '__main__':
 
     N = 3
     k = 36
+    n_classes = 1000
     D_img = (k, 2560)
     img_feats = torch.randn((N,) + D_img, dtype=torch.float)
 
@@ -71,6 +87,6 @@ if __name__ == '__main__':
     print(img_feats.shape)
     print(q_feats.shape)
 
-    model = Ramen(k, N)
+    model = Ramen(k, N, n_classes)
 
     model.forward(img_feats, q_feats)
